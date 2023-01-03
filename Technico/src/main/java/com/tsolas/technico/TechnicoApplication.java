01a471cfbf8b675118821b55e5dab359538dc052
TsolasGeorgios <tsolas16@gmail.com>
2023-01-03 18:20:37 +0200

    Added model package

---
+++ Technico/src/main/java/com/tsolas/technico/TechnicoApplication.java
@@ -0,0 +1,9 @@
+package com.tsolas.technico;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class TechnicoApplication extends Application {
+
+}
---
+++ Technico/src/main/java/com/tsolas/technico/model/PersistentClass.java
@@ -0,0 +1,18 @@
+package com.tsolas.technico.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@MappedSuperclass
+public class PersistentClass {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+}
