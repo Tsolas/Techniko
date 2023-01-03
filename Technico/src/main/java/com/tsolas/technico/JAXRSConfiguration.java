c77a585babb032106d1f9a5babdedf7c2ab7cbd2
TsolasGeorgios <tsolas16@gmail.com>
2023-01-03 17:45:38 +0200

    fixed pom.xml

--- Technico/src/main/java/com/tsolas/technico/JAXRSConfiguration.java
+++ Technico/src/main/java/com/tsolas/technico/JAXRSConfiguration.java
@@ -1,13 +1,14 @@
 package com.tsolas.technico;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
 
 /**
  * Configures JAX-RS for the application.
+ *
  * @author Juneau
  */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
-    
+
 }
--- Technico/src/main/java/com/tsolas/technico/resources/JavaEE8Resource.java
+++ Technico/src/main/java/com/tsolas/technico/resources/JavaEE8Resource.java
@@ -1,20 +1,20 @@
 package com.tsolas.technico.resources;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 /**
  *
- * @author 
+ * @author
  */
 @Path("javaee8")
 public class JavaEE8Resource {
-    
-    @GET
-    public Response ping(){
-        return Response
-                .ok("ping")
-                .build();
-    }
+
+  @GET
+  public Response ping() {
+    return Response
+            .ok("ping")
+            .build();
+  }
 }
